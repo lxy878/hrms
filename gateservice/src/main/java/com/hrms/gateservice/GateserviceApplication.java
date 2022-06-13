34c3a57888b8fbd114d9d7e312b848bd24cad734
lxy878 <lxy878@yahoo.com>
2022-06-13 14:53:03 -0400

    add services for user, admin, and gate

---
+++ gateservice/src/main/java/com/hrms/gateservice/GateserviceApplication.java
@@ -0,0 +1,13 @@
+package com.hrms.gateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateserviceApplication.class, args);
+	}
+
+}
