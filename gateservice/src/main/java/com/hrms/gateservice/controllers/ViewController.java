8af1d15da1afe9f28e19e710d49930e69edb35eb
lxy878 <lxy878@yahoo.com>
2022-06-13 22:54:03 -0400

    add index.jsp

---
+++ gateservice/src/main/java/com/hrms/gateservice/controllers/ViewController.java
@@ -0,0 +1,13 @@
+package com.hrms.gateservice.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+    
+    @GetMapping({"/", "/index","/home"})
+    private String getIndex(){
+        return "index";
+    }
+}
